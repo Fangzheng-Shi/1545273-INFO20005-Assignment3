c52dd291e0fa4353fd71ee1d9df196bdeb4a625f
Fangzheng-Shi <fangzheng.shi@student.unimelb.edu.au>
2025-06-01 17:01:52 +1000

    update 10

--- home.js
+++ home.js
@@ -45,6 +45,20 @@ function resetInterval() {
 
 resetInterval();
 
+/* Open side bar */
 function openSideBar() {
-    
+    document.getElementById("sidebar").classList.add("visible");
+    document.getElementById("overlay").classList.add("visible");
 }
+
+/* Close side bar */
+document.getElementById("closeSidebar").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.remove("visible");
+    document.getElementById("overlay").classList.remove("visible");
+});
+
+// click the overplay area to close sidebar
+document.getElementById("overlay").addEventListener("click", function () {
+    document.getElementById("sidebar").classList.remove("visible");
+    document.getElementById("overlay").classList.remove("visible");
+});
